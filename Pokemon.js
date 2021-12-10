f3dd661fce2bdbcce9b1ecd79e8315edd04aabbd
bartuczpetra <bartucz.petra@gmail.com>
2021-12-10 10:46:20 +0100

    Pokemon class

---
+++ Pokemon.js
@@ -0,0 +1,23 @@
+class Pokemon {
+    constructor(elem, adat) {
+        this.elem = elem;
+        this.adat = adat;
+
+        this.nev = this.elem.children("h2");
+        this.kep = this.elem.children("img");
+        this.kepesseg = this.elem.children("p");
+
+        this.setAdatok(this.adat);
+
+        
+
+    }
+
+    setAdatok(adat) {
+        this.nev.html(adat.forms.name);
+        this.kep.html(adat.forms.name);
+        this.kepesseg.html(adat.abilities.name);
+    }
+
+
+}
--- js.js
+++ js.js
@@ -2,6 +2,11 @@ $(function(){
     const tomb=[];
     const apiVegpont="https://pokeapi.co/api/v2/pokemon/";
     const myAsszinkron=new MyAsszinkron();
+
+    const szuloElem = $("table");
+    const sablonElem = $(".sablon");
+
+    sablonElem.remove();
     
     
     $("button").on("click", function(){
@@ -11,9 +16,9 @@ $(function(){
 
     function megjelenit(tomb){
         console.log(tomb);
-        $("h2").html(tomb.name);
+        $("h2").html(tomb.forms.name);
         $("img").attr("src",tomb.sprites.front_default);
-        $("img").attr("attr",tomb.name);
+        $("img").attr("attr",tomb.forms.name);
     }
 
     function hiba(){
@@ -21,4 +26,13 @@ $(function(){
         $("img").attr("src","https://c.tenor.com/-n8JvVIqBXkAAAAM/dddd.gif");
         $("img").attr("alt","hiba");
     }
+
+    $("#lista").on("click", function(){
+        tomb.forEach(function (adat) {
+            let ujElem = sablonElem.clone().appendTo(szuloElem);
+            const pokemon = new Task(ujElem, adat);
+            console.log(adat);
+        });
+    });
+
 });
